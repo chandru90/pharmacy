6c5aed7473870bbd218b75c6b0df0a1839615650
chandru90 <iochandru90@gmail.com>
2023-03-11 12:00:02 +0530

    second commit

---
+++ src/Availablemedicine.js
@@ -0,0 +1,36 @@
+import classes from './AvailableMeals.module.css'
+import Card from '../UI/Card'
+import MealItem from './MealItem/MealItem'
+const DUMMY_MEALS =[
+   { id:1 ,
+    name :'idly' ,
+    description : 'batter' ,
+    price :45
+   },
+   { id:2 ,
+    name :'dosa' ,
+    description : 'batter' ,
+    price :60
+   },
+   { id:3 ,
+    name :'vada' ,
+    description : 'batter' ,
+    price :30
+   }
+
+
+]
+
+
+const AvailableMeals =()=>{
+        const MealList =DUMMY_MEALS.map((meal)=> <MealItem key={meal.id} id= {meal.id} name ={meal.name} discription={meal.description} price={meal.price} /> )
+    return <section className ={classes.meals}>
+       <Card>
+       <ul>
+            {MealList}
+        </ul>
+       
+        </Card>
+    </section>
+}
+export default AvailableMeals
